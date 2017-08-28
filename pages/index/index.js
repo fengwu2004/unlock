1afd8bbd73f4d0f9e3c739c2e536d759af151fb0
晏立 <yanli@yellfun.com>
2017-08-28 13:49:41 +0800

    正式调通数据发送

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,68 +1,72 @@
 //index.js
 //获取应用实例
+
 var app = getApp()
 Page({
   data: {
     open: '开启蓝牙',
-    search:'搜索蓝牙设备',
-    getdevice:'获取蓝牙设备',
-    connect:'连接蓝牙设备',
-    connect:'连接',
+    search: '搜索蓝牙设备',
+    getdevice: '获取蓝牙设备',
+    connect: '连接蓝牙设备',
+    connect: '连接',
     senddata: '发送数据',
-    receivedata:'读取蓝牙数据',
-    result:'未连接',
-    deviceId:'',
-    servierId:'',
-    writecharacteristicId:'',
+    receivedata: '读取蓝牙数据',
+    result: '未连接',
+    deviceId: '',
+    servierId: '',
+    writecharacteristicId: '',
     indicatecharacteristicId: '',
+    beginReceive: false,
+    receivechars: '',
+    receiveLength: 0,
     devices: [],
     userInfo: {}
   },
   //事件处理函数
-  openDevice: function() {
+  openDevice: function () {
     var self = this
     wx.openBluetoothAdapter({
-      success: function(res) {
+      success: function (res) {
         self.setTip('开启成功')
       },
-      fail: function(res) {
+      fail: function (res) {
         self.setTip('开启蓝牙失败')
       }
     })
   },
-  setTip: function(str) {
+  setTip: function (str) {
     this.setData({
       result: str
     })
   },
-  searchDevice: function() {
+  searchDevice: function () {
 
     var self = this
 
     wx.startBluetoothDevicesDiscovery({
-      services:['FEE7'],
-      success: function(res) {
+      services: ['FEE7'],
+      success: function (res) {
         self.setTip('搜索成功')
       },
-      fail:function (res) {
+      fail: function (res) {
         self.setTip('搜索失败')
       }
     })
   },
-  getDevice: function() {
+  getDevice: function () {
     var self = this
     wx.getBluetoothDevices({
-      success: function(res) {
+      success: function (res) {
         console.log(JSON.stringify(res))
         self.setData({
           result: '获取蓝牙设备成功',
-          devices:res.devices
+          devices: res.devices
         })
       },
-      fail: function(res) {
+      fail: function (res) {
         console.log(JSON.stringify(res))
       },
-      complete: function(res) {
+      complete: function (res) {
         console.log(JSON.stringify(res))
       }
     })
@@ -71,22 +75,13 @@ Page({
 
     var self = this
 
-    this.data.deviceId = ''
-
-    if (this.data.devices.length <= 0) {
-
-      self.setTip('请先点击获取蓝牙设备')
-
-      return
-    }
-
-    this.data.deviceId = this.data.devices[0].deviceId
+    this.data.deviceId = 'F4:B8:5E:DD:8F:6E'
 
     wx.createBLEConnection({
 
       deviceId: self.data.deviceId,
 
-      success: function(res) {
+      success: function (res) {
 
         wx.getBLEDeviceServices({
 
@@ -147,7 +142,50 @@ Page({
       },
     })
   },
-  sendData: function (){
+
+  splictPackage: function (str) {
+
+    var totallength = str.length + 8
+
+    var packs = []
+
+    var buffer = new ArrayBuffer(totallength)
+
+    var dv = new DataView(buffer)
+
+    dv.setUint8(0, 0xFE)
+    dv.setInt8(1, 1)
+    dv.setUint16(2, str.length)
+    dv.setUint16(4, 0)
+    dv.setUint16(6, 0)
+
+    for (var i = 0; i < str.length; ++i) {
+
+      dv.setUint8(i + 8, str.charCodeAt(i))
+    }
+
+    var packcount = Math.ceil(totallength / 20)
+
+    for (var i = 0; i < packcount; ++i) {
+
+      var pack = new ArrayBuffer(20)
+
+      var dataview = new DataView(pack)
+
+      for (var j = 0; j < 20 && j + i * 20 < totallength; ++j) {
+
+        var value = dv.getUint8(j + i * 20)
+
+        dataview.setUint8(j, value)
+      }
+
+      packs.push(pack)
+    }
+
+    return packs
+  },
+
+  sendData: function () {
 
     var self = this
 
@@ -158,34 +196,47 @@ Page({
       return
     }
 
-    var str = 'helloworld'
+    var str = '11111111112222222222333333333344'
 
-    var buffer = new ArrayBuffer(str.length)
+    var packs = self.splictPackage(str)
 
-    var dataview = new DataView(buffer)
+    function queueSend(pack) {
 
-    for (var i = 0; i < str.length; ++i) {
+      return new Promise(function (resolve, reject) {
 
-      dataview.setUint8(i, str.charCodeAt(i))
+        wx.writeBLECharacteristicValue({
+          deviceId: self.data.deviceId,
+          serviceId: self.data.serverId,
+          characteristicId: self.data.writecharacteristicId,
+          value: pack,
+          complete: function (res) {
+            // console.log('发送包:' + index)
+            resolve(res)
+          },
+          fail: function (res) {
+            // console.log('发送包:' + index + '失败')
+            reject(res)
+          }
+        })
+      })
     }
 
-    wx.writeBLECharacteristicValue({
+    var result = Promise.resolve()
 
-      deviceId: self.data.deviceId,
+    packs.forEach((pack) => {
 
-      serviceId: self.data.serverId,
+      result = result.then(() => {
 
-      characteristicId: self.data.writecharacteristicId,
+        return queueSend(pack)
+      })
+    })
 
-      value: buffer,
+    result.then(() => {
 
-      success: function (res) {
-
-        console.log('writeBLECharacteristicValue success', res.errMsg)
-      }
+      self.setTip('发送完毕')
     })
   },
-  receiveData: function() {
+  receiveData: function () {
 
     var self = this
 
@@ -207,6 +258,7 @@ Page({
       characteristicId: self.data.indicatecharacteristicId,
 
       success: function (res) {
+
         self.setTip('notifyBLECharacteristicValueChange success', res.errMsg)
       }
     })
@@ -215,24 +267,47 @@ Page({
 
       var buffer = res.value
 
-      var arr = new Uint8Array(buffer);
+      var arr = new Uint8Array(buffer)
 
-      var str = String.fromCharCode.apply(String, arr);
+      var dv = new DataView(buffer)
 
-      self.setTip('读取到的值是' + ':' + str)
+      if (self.receiveLength == 0) {
+
+        self.receiveLength = dv.getUint16(2)
+
+        for (var i = 0; i < self.receiveLength && i < 12; ++i) {
+
+          self.receivechars += String.fromCharCode(dv.getUint8(8 + i))
+        }
+
+        console.log('接收到的字符串:' + self.receivechars)
+
+        return
+      }
+      
+      for (var i = 0; i < arr.length; ++i) {
+
+        self.receivechars += String.fromCharCode(dv.getUint8(i))
+      }
+      
+      console.log('接收到的字符串:' + self.receivechars)
     })
 
     wx.readBLECharacteristicValue({
-      
+
       deviceId: self.data.deviceId,
-      
+
       serviceId: self.data.serverId,
-      
+
       characteristicId: self.data.indicatecharacteristicId,
 
       success: function (res) {
 
         self.setTip(writecharacteristicId + ':' + JSON.stringify(res))
+      },
+      complete: function () {
+
+        self.receiveLength = 0
       }
     })
   },
@@ -240,10 +315,10 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
